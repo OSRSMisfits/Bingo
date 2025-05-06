fa5856a35d2a2a85e5c21600c3b9983e92cbdb74
Riley Mollon <rmollon7@yahoo.com>
2025-05-05 22:56:32 -0700

    Fix build issues

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,27 @@
 /// <reference types="vite/client" />
+
+interface GameDetails {
+    details: BoardDetails,
+    board: Array<Array<Tile>>
+}
+
+interface BoardDetails {
+    startTime: Date,
+    endTime: Date,
+    gameType: number,
+    boardSize: number,
+    teamCount: number,
+    teams: Array<Team>
+}
+
+interface Team {
+    name: string,
+    members: Array<string>
+}
+
+interface Tile {
+    image: string,
+    name: string,
+    description: string,
+    points: number
+}
