d1cf00e35d03a8404aac02cb853dd61a3467f088
Riley Mollon <rmollon7@yahoo.com>
2025-05-22 11:06:58 -0700

    Add rules

--- src/App.vue
+++ src/App.vue
@@ -282,6 +282,25 @@
 
     return `${days}d ${hours}h ${minutes}m ${seconds}s`
   })
+
+  const renderRules = ref(false)
+  const showRules = ref(false)
+
+  function openRules() {
+    if (inspectData.value.inspecting) return
+
+    renderRules.value = true
+    showRules.value = true
+  }
+
+  function closeRules() {
+    renderRules.value = false
+    setTimeout(() => showRules.value = false, 200)
+  }
+
+  function moveRules() {
+    return window.innerWidth <= 725
+  }
 </script>
 
 <template>
@@ -338,6 +357,16 @@
         </div>
       </div>
     </div>
+  </div>
+
+  <div :class="{ 'rules-button': true, 'move': moveRules() }" @click="openRules()">Rules</div>
+  <div :class="{ 'rules-overlay': true, show: renderRules, background: !showRules }" @click="closeRules">
+    <ul class="rules-list">
+      <li class="rule">You will be allowed to use up to 2 accounts for this competition. For example, You have an ironman that I’ve already completed the content/boss that is needed for said tile. You can use the other account to complete the tile. However, multi-logging the same content is not allowed. You cannot be using two separate accounts at the same time trying to get tiles done. This will result in disqualification from bingo.</li>
+      <li class="rule">There will be a secret code released when the event starts that must be in all screenshots for them to count. There is a plugin for Wise Old Man that I would highly recommend installing if you do not have it already. This plugin has a section where you insert the codeword and it will stay on screen. This is not needed but very nice to have so you don't have to remember to input the code before taking every screenshot. If the code is not in the screenshot. The drop will not count towards updating your board. “Clan Events” plug-in can also be used.</li>
+      <li class="rule">You can run content with other teams and still mark off tiles on your bingo card. For example, Player A is on Team 1 and Player B is on Team 2. They end up doing some type of content together. Player A pulls the drop. Player B can claim the tile completed, but you need a screenshot of you being there with the drop in the chat box. However, If you run content with people from outside of the clan and they pull the drop it does not count</li>
+      <li class="rule">Every team will have their own channel in the discord where you can chat about strategies and post drops. You can only use a drop to complete one tile. No drop can be used twice. However, if you repull the same drop it can be used. example: Huey kc 20 - tome, then Huey kc 32- tome you can use both drops.<br/><strong>For tiles like XP gained or Kc. please post a pic of your beginning XP or Kc & ending XP or Kc.</strong></li>
+    </ul>
   </div>
 
   <div v-if="renderStartsIn" class="starts-in">
@@ -693,6 +722,86 @@
     z-index: 10000;
     font-size: 20px;
     user-select: none;
+  }
+
+  .rules-button {
+    position: absolute;
+    z-index: 10000;
+    right: 25px;
+    top: 20px;
+
+    height: 25px;
+    line-height: 25px;
+    padding: 5px 15px;
+
+    background: #460875;
+    border: 2px solid #161616;
+    border-radius: 5px;
+
+    cursor: pointer;
+    transition: background 0.2s ease-in-out;
+  }
+
+  .rules-button:hover {
+    background: #560a91;
+  }
+
+  .rules-button.move {
+    top: 180px;
+    background: #161616;
+    border: 2px solid #030303;
+  }
+
+  .rules-button.move:hover {
+    background: #0c0c0c;
+  }
+
+  .rules-overlay {
+    position: fixed;
+    top: 172px;
+    background: rgba(0, 0, 0, 0.9);
+    width: 100%;
+    height: 100%;
+    left: -16px;
+    padding-right: 16px;
+    z-index: 500;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+  }
+
+  .rules-overlay .contents {
+    top: 0;
+    position: relative;
+    transition: top 0.2s ease-in-out;
+
+    width: 100%;
+    margin-left: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .rules-overlay.show .contents {
+    top: 20px;
+  }
+
+  .rules-overlay.show {
+    opacity: 1;
+  }
+
+  .rules-overlay.background {
+    z-index: -500;
+  }
+
+  .rules-overlay .rules-list {
+    width: 50%;
+    margin-left: 25%;
+    height: calc(90% - 200px);
+    overflow-y: auto;
+  }
+
+  .rules-overlay .rules-list .rule {
+    margin-top: 40px;
   }
 
   .boards {
